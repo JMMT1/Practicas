47592407c4a73aa8febba058fcbd7d909e3d6f11
Manuel Molinar <90782338+JMMT1@users.noreply.github.com>
2021-11-04 08:52:47 -0700

    Add files via upload

---
+++ Eva2_5_FIBONACCI/src/eva2_5_fibonacci/Eva2_5_FIBONACCI.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_5_fibonacci;
+
+/**
+ *
+ * @author Admin
+ */
+public class Eva2_5_FIBONACCI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static int fibonacci(int iPos){
+        return 0;
+        
+    }
+    
+}

f370d45f04ecaf73904f3d7ab5d023684f84fb8f
Manuel Molinar <90782338+JMMT1@users.noreply.github.com>
2021-11-10 08:54:05 -0700

    Add files via upload

---
+++ EVA2_7_LISTA_SIMPLE/src/eva2_7_lista_simple/Lista.java
@@ -0,0 +1,35 @@
+package eva2_7_lista_simple;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Lista {//Lista simplemente enlazada (Linked List)
+    private Nodo inicio;//Acceso a mi lista, punto de partida
+    
+    public Lista(){
+        inicio = null;//Lista Vacia
+    }
+    
+    //Agegar nodos
+    public void add(Nodo nuevo){
+        //Hay nodos en la lista?
+        if(inicio == null){//Lista vacia
+            inicio = nuevo;//conectamos el primer nodo a la lista
+        }else{//Lista con nodos
+            //Hay que movernos al final de la lista
+            //Usando TEMP, Nodo por Nodo hasta llegar al final
+            Nodo temp = inicio;//aqui empezamos
+            //WHILE
+            while(){//Mientras TEMP != Null vamos a mover
+            
+        }
+    }
+    //Imprimir la lista
+}
